37a9164e8445807778722573bbcfd5b004fd257d
kaylebredner <70593884+kaylebredner@users.noreply.github.com>
2022-04-19 10:13:45 -0400

    Add files via upload

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +12,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
+import java.io.FileNotFoundException;
 
 
 public class Main extends Application {
@@ -20,14 +23,31 @@ public class Main extends Application {
 	Label lblSearch = new Label("Search");
 	TextField txtSearch = new TextField();
 	ComboBox<String> cbxCategory = new ComboBox<>();
-	
+
+	//Item Containers
+
+
+
 	//Containers
 	HBox hbxMenu = new HBox(10,btnRainforest,btnCart,lblSearch,cbxCategory,txtSearch);
+	HBox[] hbxItemRow = new HBox[10];
+	Product product;
+
+	{
+		try {
+			product = new Product();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	VBox vbxItems = new VBox(10,product.mainBox);
+
 	public void start(Stage primaryStage) {
 		try {
-			cbxCategory.getItems().addAll("All Items");
+			cbxCategory.getItems().addAll("All Items","Electronics","Grocery");
 			cbxCategory.getSelectionModel().select(0);
-			BorderPane root = new BorderPane(hbxMenu);
+			BorderPane root = new BorderPane(vbxItems);
 			Scene scene = new Scene(root,1200,700);
 			primaryStage.setTitle("Rainforest");
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
---
+++ src/application/Product.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Product {
+    VBox mainBox = new VBox();
+
+    ImageView productImage = new ImageView();
+    Image image;
+
+    {
+        try {
+            image = new Image(new FileInputStream("default-product-image.png"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Label lblPrice = new Label("$0.00");
+    Label lblName = new Label();
+    Button btnPurchase = new Button("Purchase");
+    HBox  pricePurchase = new HBox(10,lblPrice,btnPurchase);
+
+    Product() throws FileNotFoundException {
+        lblName.setText("ItemNotFound");
+        mainBox.getChildren().add(productImage);
+        mainBox.getChildren().add(lblName);
+        mainBox.getChildren().add(pricePurchase);
+        productImage.setImage(image);
+    }
+}

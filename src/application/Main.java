dd400c134b0d457503fd8a03338e7bc1f74748bb
kaylebredner <rednkd01@pfw.edu>
2022-04-21 20:20:07 -0400

    Created page of products
    ProductList class is incomplete

--- src/application/Main.java
+++ src/application/Main.java
@@ -18,41 +18,12 @@ import java.io.FileNotFoundException;
 
 public class Main extends Application {
 
-	//controls
-	Button btnRainforest = new Button("Rainforest");
-	Button btnCart = new Button("Cart");
-	Label lblSearch = new Label("Search");
-	TextField txtSearch = new TextField();
-	ComboBox<String> cbxCategory = new ComboBox<>();
-	Button btnOrders = new Button("Orders");
-
-
-	//Item Containers
-
-
-
-	//Containers
-	HBox hbxMenu = new HBox(10,btnRainforest,btnCart,btnOrders,lblSearch,cbxCategory,txtSearch);
-	Product[][] product = new Product[3][3];
-
-	GridPane itemGrid = new GridPane();
-	VBox vbxItems = new VBox(10,hbxMenu);
-
 
 
 	public void start(Stage primaryStage) {
 		try {
-			hbxMenu.setAlignment(Pos.CENTER);
-
-			vbxItems.setAlignment(Pos.TOP_CENTER);
-			itemGrid.setAlignment(Pos.CENTER);
-			btnOrders.setPrefWidth(100);
-			BuildProducts();
-			btnRainforest.addEventHandler(MouseEvent.MOUSE_CLICKED,rainforestHandler);
-			btnCart.addEventHandler(MouseEvent.MOUSE_CLICKED,cartHandler);
-			cbxCategory.getItems().addAll("All Items","Electronics","Grocery");
-			cbxCategory.getSelectionModel().select(0);
-			BorderPane root = new BorderPane(vbxItems);
+			ProductList products = new ProductList();
+			BorderPane root = new BorderPane(products.getNode());
 			Scene scene = new Scene(root,1200,700);
 			primaryStage.setTitle("Rainforest");
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -66,41 +37,6 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	public void BuildProducts() throws FileNotFoundException{
-		if(!itemGrid.getChildren().isEmpty()){
-			clearProducts();
-		}
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				product[i][j]=new Product();
-				itemGrid.add(product[i][j].getNode(),j,i);
-
-			}
-		}
-		vbxItems.getChildren().add(itemGrid);
-		itemGrid.setAlignment(Pos.CENTER);
-
-	}
-	public  void clearProducts(){
-		itemGrid.getChildren().clear();
-		vbxItems.getChildren().remove(itemGrid);
-	}
-	public void clearPage(){
-		vbxItems.getChildren().clear();
-		vbxItems.getChildren().add(hbxMenu);
-	}
-	EventHandler<MouseEvent> cartHandler = event -> {
-		Cart newCart = new Cart();
-		clearProducts();
-		clearPage();
-		vbxItems.getChildren().add(newCart.getCart());
-	};
-	EventHandler<MouseEvent> rainforestHandler = event -> {
-		try {
-			clearPage();
-			BuildProducts();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	};
+	
+	
 }

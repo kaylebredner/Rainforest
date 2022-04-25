4331371ecba91e73d546e01087416245ec16f057
Alex Bishop <64106114+AlexBishop01@users.noreply.github.com>
2022-04-25 02:58:16 -0400

    added "search by customer name" query

--- mySQL/shippingLocationQueries.sql
+++ mySQL/shippingLocationQueries.sql
@@ -1,8 +1,19 @@
 use rainforest;
 
+
+
 /* RETURN ALL SAVED SHIPMENT LOCATIONS
 
 SELECT * FROM shipmentLocation_t;
+
+*/
+
+/* SEARCH FOR SHIPPING LOCATION BY CUSTOMER NAME
+
+SELECT shipmentLocation_t.LocationName, shipmentLocation_t.Address1, shipmentLocation_t.Address2, shipmentLocation_t.City, shipmentLocation_t.State, shipmentLocation_t.ZipCode
+	FROM shipmentLocation_t INNER JOIN customer_t
+		ON shipmentLocation_t.LocationID = customer_t.CustomerID
+			WHERE shipmentLocation_t.LocationName LIKE '%x%'; (<--- X is the name of the customer you are searching for)
 
 */
 
